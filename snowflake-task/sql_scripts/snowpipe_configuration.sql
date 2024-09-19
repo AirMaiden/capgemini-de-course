2b60cc804ce047b15f12c9378bf9d4e047921add
AirMaiden <evzheni@gmail.com>
2024-09-19 19:31:48 +0300

    Added a comment to replace aws variables.

--- snowflake-task/sql_scripts/snowpipe_configuration.sql
+++ snowflake-task/sql_scripts/snowpipe_configuration.sql
@@ -4,7 +4,7 @@
 USE DATABASE nyc_airbnb;
 USE SCHEMA public;
 
--- Step 2: Create the external stage pointing to the S3 bucket
+-- Step 2: Create the external stage pointing to the S3 bucket (don't forget to replace aws variables with real values for your bucket)
 CREATE OR REPLACE STAGE nyc_airbnb_stage
 URL = 's3://<AWS_BUCKET_NAME>/'
 CREDENTIALS = (AWS_KEY_ID = '<AWS_ACCESS_KEY>' AWS_SECRET_KEY = '<AWS_SECRET_KEY>');
